7beb1863bcbc7db4f2f69af2541983118e45ba47
he445 <103337053+he445@users.noreply.github.com>
2023-01-16 14:09:26 -0300

    feat: first commi

---
+++ react-ts/src/navBar/navbar.tsx
@@ -0,0 +1,12 @@
+import Modal from 'react-modal';
+import { useEffect, useState } from 'react';
+
+Modal.setAppElement('#root');
+ function navbar(){
+    return (
+        <div>
+            <h1>Diario</h1>
+        </div>
+    )
+ }
+ export default navbar;
